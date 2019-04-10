0ff50ae351acdf2b7c382c9f17296496e5515503
Austin Beaufort <tinyhatcoding@gmail.com>
2019-04-09 22:17:47 -0400

    Updates

---
+++ precache-manifest.b31b3b5464cea44fe923050079854c44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfc848dacff88822b4f1",
+    "url": "/formal-site/static/css/main.79cbe611.chunk.css"
+  },
+  {
+    "revision": "bfc848dacff88822b4f1",
+    "url": "/formal-site/static/js/main.45321630.chunk.js"
+  },
+  {
+    "revision": "98a151ab13f0c289b481",
+    "url": "/formal-site/static/js/runtime~main.5d069483.js"
+  },
+  {
+    "revision": "9184e3f6533ea52f26eb",
+    "url": "/formal-site/static/js/2.1b1179e5.chunk.js"
+  },
+  {
+    "revision": "1c8295765ef2671529d540c515419364",
+    "url": "/formal-site/static/media/logo.1c829576.png"
+  },
+  {
+    "revision": "c111c938ce23bb379914b4b3872d76e0",
+    "url": "/formal-site/index.html"
+  }
+];
